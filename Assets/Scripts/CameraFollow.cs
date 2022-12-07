0119443c1aef488d7fa07546a9ea57f24d96b379
BelieverQ <118184514+BelieverQ@users.noreply.github.com>
2022-12-07 23:03:56 +0100

    update art 221207
    已替换sprites：背景，角色，植物的四个状态，背包图标（目前文本全用TextMeshPro，Material Preset选Drop Shadow就行），升级和道具购买的board
    上传但尚未使用的sprites：浓雾（覆盖无法耕种的田地，在farmland文件夹），道具和升级的图标（icons文件夹），变异植物的3个状态（plants-hairAlgae文件夹）
    微调：角色大小及Rigidbody，walls的位置及collider相对位置，cells的位置及collider相对位置，修改了cellssprites的order in layer
    摄像机：在camera的script中增加了根据walls设置的摄像机范围

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,17 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     public Character characterScript;
+    
+        public Object wallLeft;
+        public Object wallRight;
+        public Object wallUp;
+        public Object wallDown;
+    
+        //for Mathf.Clamp, setting the boundary for Camera
+        public float leftModify;
+        public float rightModify;
+        public float upModify;
+        public float downModify;
+        private float leftBoundary;
+        private float rightBoundary;
+        private float upBoundary;
+        private float downBoundary;
+    
+        void Awake()
+        {
+            leftBoundary = wallLeft.GetComponent<Transform>().position.x +
+                           wallLeft.GetComponent<BoxCollider2D>().size.x / 2 + 
+                           gameObject.GetComponent<Camera>().orthographicSize * Screen.width / Screen.height + leftModify;
+            rightBoundary = wallRight.GetComponent<Transform>().position.x -
+                           wallRight.GetComponent<BoxCollider2D>().size.x / 2 - 
+                           gameObject.GetComponent<Camera>().orthographicSize * Screen.width / Screen.height + rightModify;
+            upBoundary = wallUp.GetComponent<Transform>().position.y - wallUp.GetComponent<BoxCollider2D>().size.y / 2 - 
+                gameObject.GetComponent<Camera>().orthographicSize + upModify;
+            downBoundary = wallDown.GetComponent<Transform>().position.y + wallDown.GetComponent<BoxCollider2D>().size.y / 2 + 
+                           gameObject.GetComponent<Camera>().orthographicSize + downModify;
+        }
 
     // Start is called before the first frame update
     void Start()
     {
         characterScript.characterPosChanged = (Transform characterTransform) =>
         {
-            Vector3 newPos = new(characterTransform.position.x, characterTransform.position.y, transform.position.z);
+            Vector3 newPos = new(Mathf.Clamp(characterTransform.position.x, leftBoundary, rightBoundary), Mathf.Clamp(characterTransform.position.y, downBoundary, upBoundary), transform.position.z);
             transform.position = newPos;
         };
     }
